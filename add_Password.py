b78d2510d07167eac38027850fcd6472f80ea14a
EnzoMattio <113895326+EnzoMattio@users.noreply.github.com>
2023-01-30 09:44:42 +0100

    Changement de quelque textes

--- add_Password.py
+++ add_Password.py
@@ -1,7 +1,10 @@
-import tkinter as tk
 from tkinter import *
 import customtkinter as ctk
-import random
+from tkinter import messagebox
+import hashlib
+import json
+
+voir_ident = False
 
 input = ""
 car_spécial = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")",  "[", "]", "|", ":", ";", "-", "_", "+", "=", "{", "}", "<", ">", ",", ".", "?"]
@@ -11,8 +14,29 @@ GRIS = ""
 VERT_BRUUU = "#436D35"
 VERT_BRUUU_CLAIR = "#799C65"
 
-attest_mdp = ""
+ 
+with open ("mdp.json", "r") as fichier:
+  stock = json.load(fichier)
 
+master = Tk()
+master.title("Crypteur de mot de passe")
+master.geometry("400x400")
+master.resizable(False, False)
+master.config()
+zomb = PhotoImage(file="BRUUUU.png")
+output = StringVar()
+json_mdp = StringVar()
+id_liste = Listbox()
+
+label1 = Label(master, i=zomb)
+label1.pack()
+label1.place(
+  x = 0,
+  y = 0, 
+
+  height=400,
+  width=400
+  )
 
 # Fonction qui permet de vérifier les conditions qu'on veut pour le mot de passe.
 
@@ -38,59 +62,124 @@ def verif_password(mot_de_passe):
   else:
     return False
   
-  if contient_chiffre == True and contient_majuscule == True and contient_minuscule == True and contient_special == True:
-    attest_mdp = "Mot de passe valide"
-   
   print(contient_special)
   print(contient_chiffre)
   print(contient_majuscule)
   print(contient_minuscule)
   
-  
+  if contient_chiffre == True and contient_majuscule == True and contient_minuscule == True and contient_special == True:
+    output.set("Mot de passe valide")
+    return True
+  else:
+    output.set("Vérifier le mot de passe")
+    return False
+   
+# print(verif_password("zdvhjbvzeion"))
+
+def hash_pass(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
   
 def add():
-  password = motdepasse.get()
-  verif_password(password)
+  
+  mdp = motdepasse.get()
+  identifiants = identifiant.get()
+  passw = hash_pass(mdp)
+  if not verif_password(mdp):
+    messagebox.showerror("Invalide",
+      "Mot de passe invalide, Il doit contenir au minimum 8 carctères avec 1 majuscules, 1 Minuscules, 1 Chiffre et 1 caractère spécial.")
+    return
   
   
-# print(verif_password("zdvhjbvzeion"))
-master = Tk()
-master.title("Crypteur de mot de passe")
-master.geometry("400x400")
-master.config(
-  bg= VERT_BRUUU
+  if identifiants not in [i for i in stock] and passw not in [stock[i] for i in stock]:
+    stock[identifiants]=passw
+    with open("mdp.json", 'w') as fichier:
+      json.dump(stock, fichier, indent=4)
+    messagebox.showinfo("Réussi",
+    "Pseudo et Mot de passe, sauvegardé")
+  else:
+    messagebox.showerror("Utilisé",
+    "Username et/ou password déjà utilisé.")
+
+
+  with open("mdp.json", "w") as fichier:
+    json.dump(stock, fichier)
+
+
+
+def Voir():
+  global voir_ident, ident, id_liste
+
+  
+  if voir_ident:
+    id_liste.destroy()
+    voir_ident = False
+  else:
+    id_liste = Listbox(master, fg="black", bg="light gray")
+    id_liste.place(x=0, y=250, width=400, height=150)
+
+    for key, value in stock.items():
+        id_liste.insert(END, "Pseudo : " + key ) 
+        id_liste.insert(END, "Mot de passe : " + value )
+    voir_ident = True  
+
+def on_clear():
+  
+  for i in stock:
+    stock.pop(i)
+  with open("mdp.json", "w") as fichier:
+    json.dump(stock, fichier)
+  
+  id_liste.delete(0, END)
+
+identifiant = ctk.CTkEntry(
+  master,
+  border_color = NOIR,
+  corner_radius= 2,
+  text_color= NOIR,
+  fg_color="transparent",
+  justify=CENTER,
+  font=("arial", 20),
 )
 
-
-bg = PhotoImage(file="BRUUUU.png")
-label1 = Label( 
-  master, 
-  image = bg
-  )
-label1.place(
-  x = 0,
-  y = 0, 
-  height=400,
-  width=400
-  )
+identifiant.place(
+  x= 50,
+  y= 50,
+  width= 300,
+  height= 50,
+)
 
 
 
 motdepasse = ctk.CTkEntry(
   master,
   border_color = NOIR,
-  corner_radius= 15,
+  corner_radius= 2,
   text_color= NOIR,
   fg_color="transparent",
   justify=CENTER,
-  font=("arial", 25),
+  font=("arial", 20),
 )
 
 motdepasse.place(
   x= 50,
-  y= 50,
+  y= 100,
   width= 300,
-  height= 100,
+  height= 50,
+)
+
+bouton_clear = ctk.CTkButton(
+  master,
+  corner_radius=2,
+  fg_color=NOIR,
+  text="Effacer dernier ID",
+  command = on_clear
+)
+bouton_clear.place(
+  x=100, 
+  y=0, 
+  height=50,
+  width=200
 )
 
 bouton_envoie_mdp = ctk.CTkButton(
@@ -112,7 +201,8 @@ bouton_voir_mdp = ctk.CTkButton(
   master,
   corner_radius=2,
   fg_color=NOIR,
-  text="Voir MDP"
+  text="Voir MDP",
+  command=Voir
 )
 
 bouton_voir_mdp.place(
@@ -122,11 +212,11 @@ bouton_voir_mdp.place(
   width=100
 )
 
-texte_mdp = ctk.CTkEntry(
+texte_mdp = ctk.CTkLabel(
   master,
   text_color=NOIR,
-  textvariable= attest_mdp,
-  fg_color="transparent",
+  textvariable=output,
+  fg_color=VERT_BRUUU_CLAIR,
   justify=CENTER
 )
 texte_mdp.place(
@@ -135,4 +225,6 @@ texte_mdp.place(
   height=50,
   width=200
 )
+
+
 master.mainloop()
